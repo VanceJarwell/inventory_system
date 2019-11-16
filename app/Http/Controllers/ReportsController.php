a50307f285a9e2aed57d80e1773118482e4e3f5d
ite19-3c-19@M307.uc-bcf.edu.ph <ite19-3c-19@M307.uc-bcf.edu.ph>
2019-11-16 08:09:50 +0800

    update

---
+++ app/Http/Controllers/ReportsController.php
@@ -0,0 +1,30 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+use PhpOffice\PhpWord\TemplateProcessor;
+
+class ReportsController extends Controller
+{
+    public function word()
+    {
+    	$templateProcessor = new TemplateProcessor('./templates/Certificate of Recognition.docx');
+    	$templateProcessor->setValue('first_name', 'Thor');
+    	$templateProcessor->setValue('last_name', 'Igop');
+    	$templateProcessor->saveAs('Vance.docx');
+    	return response()->download('TVance.docx');
+
+    }
+
+    public function excel()
+    {
+    	$spreadsheet = \PhpOffice\PhpSpreadsheet\IOFactory::load('./templates/form138.xlsx');
+		$worksheet = $spreadsheet->getActiveSheet();
+		$worksheet->getCell('A7')->setValue('Name: Juan');
+		$worksheet->getCell('A8')->setValue('11-B');
+		$writer = \PhpOffice\PhpSpreadsheet\IOFactory::createWriter($spreadsheet, 'Xls');
+		$writer->save('form138.xls');
+		return response()->download('Form.xls');
+    }
+}
---
+++ resources/views/welcome.blade.php
@@ -0,0 +1,4 @@
+@extends('layouts.master')
+@section('content')
+    <div class="container"></div>
+@endsection
